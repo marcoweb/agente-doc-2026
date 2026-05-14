3464d70299e732e3cacb37bd06bdd218908f0454
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2026-05-14 23:40:36 +0000

    Aula 14/05/2025

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from pypdf import PdfReader
 import requests
+import os
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
-MODEL = "qwen2.5:0.5b"
+#OLLAMA_URL = "http://localhost:11434/api/generate"
+#MODEL = "qwen2.5:0.5b"
+
+GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
+MODEL = "llama-3.1-8b-instant"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 def extrair_texto_pdf(pdf_path):
     reader = PdfReader(pdf_path)
@@ -31,17 +36,30 @@ def perguntar_ollama(pergunta, texto):
     """
 
     resposta = requests.post(
-        OLLAMA_URL,
+        #OLLAMA_URL,
+        GROQ_URL,
+        headers={
+            "Authorization" : f"Bearer {GROQ_API_KEY}",
+            "Content-Type": "application/json"
+        },
         json={
             "model": MODEL,
-            "prompt" : prompt,
-            "stream" : False
+            # "prompt" : prompt,
+            # "stream" : False
+            "messages": [
+                {
+                    "role" : "user",
+                    "content": prompt
+                }
+            ],
+            "temperature" : 0.2
         },
         timeout=120
     )
 
     resposta.raise_for_status()
-    return resposta.json()['response']
+    #return resposta.json()['response']
+    return resposta.json()["choices"][0]["message"]["content"]
 
 def main():
     texto_pdf = extrair_texto_pdf("./regras_uno.pdf")
